e837041d554dbcb245345720a0e47c4878eba66e
Aslan <aslan.aslan.14@bk.ru>
2023-04-24 02:12:03 +0600

    Task4 done and add change in Folder Task2

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Insert number: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+while (number > 1)
+{
+    if (number % 2 == 0)
+    {
+        System.Console.WriteLine(number);
+        number = number - 1;
+    }
+    else
+    {
+        number = number - 1;
+    }
+}
